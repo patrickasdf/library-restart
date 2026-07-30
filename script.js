0ef3062ef0b39a41c9ad83c5b2301541034e86ec
patrickasdf <173692702+patrickasdf@users.noreply.github.com>
2026-07-30 20:30:37 +1000

    refactor Book from constructor to class

--- script.js
+++ script.js
@@ -2,32 +2,30 @@ const myLibrary = [];
 const bookDisplay = document.querySelector(".grid-container")
 const dialog = document.querySelector("dialog")
 
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call contstructor");
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
+        this.info = function() {
+            return(`${this.title} by ${this.author}, ${this.pages} pages. ${this.read}`)
+        };
+        this.idInfo = function() {
+            return(this.id)
+        };
+        this.infoReadStatus = function () {
+            return(this.read)
+        };
     }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
-    this.info = function() {
-        return(`${this.title} by ${this.author}, ${this.pages} pages. ${this.read}`)
-    };
-    this.idInfo = function() {
-        return(this.id)
-    };
-    this.infoReadStatus = function () {
-        return(this.read)
-    };
-}
-
-Book.prototype.toggleRead = function() {
-    if (this.read == "read") {
-        this.read = "not read yet";
-        return;
+    toggleRead() {
+        if (this.read == "read") {
+            this.read = "not read yet";
+            return;
+        }
+        else return this.read = "read";
     }
-    else return this.read = "read";
 }
 
 function addBookToLibrary(title, author, pages, read) {
